7bcc14a0e921f973278578ad425fbd41714d1ea1
raazeshp96 <raazeshp96@gmail.com>
2021-02-04 15:25:56 +0545

    setting up database config

---
+++ Config/Database.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+)
+
+var DB *gorm.DB
+
+type DBConfig struct {
+	Host     string
+	Port     int
+	User     string
+	DBName   string
+	Password string
+}
+
+func BuildDBConfig() *DBConfig {
+	dbConfig := DBConfig{
+		Host:     "localhost",
+		Port:     3306,
+		User:     "rajesh",
+		DBName:   "kowapets",
+		Password: "password",
+	}
+}
+
+func DbURL(dbConfig *DBConfig) string {
+	return fmt.Sprintf(
+		"%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local",
+		dbConfig.User,
+		dbConfig.Password,
+		dbConfig.Host,
+		dbConfig.Port,
+		dbConfig.DBName,
+	)
+}
