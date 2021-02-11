c60e4ec87ba391f5389e2e68a191aaf4383f1ff6
raazeshp96 <raazeshp96@gmail.com>
2021-02-11 14:39:52 +0545

    correcting routes

--- routes/Routes.go
+++ routes/Routes.go
@@ -1,17 +1,19 @@
 package routes
 
 import (
+	"github.com/RaazeshP96/todo_app_using_gin/controllers"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
-	v1:=r.Group("/v1"){
-		v1.GET("todo",Controllers.GetTodos)
-		v1.POST("todo",Controllers.CreateATodo)
-		v1.GET("todo/:id",Controllers.UpdateATodo)
-		v1.DELETE("todo/:id",Controllers.DeleteATodo)
-		v1.PUT("todo/:id",Controllers.update)
+	v1 := r.Group("/v1")
+	{
+		v1.GET("todo", controllers.GetTodos)
+		v1.POST("todo", controllers.CreateATodo)
+		v1.GET("todo/:id", controllers.GetATodo)
+		v1.DELETE("todo/:id", controllers.DeleteATodo)
+		v1.PUT("todo/:id", controllers.UpdateATodo)
 
 	}
 	return r
